edc6035efae163e8c282e4ac132dcafbee33989b
Kazuhiro Homma <kazu.homma@gmail.com>
2020-05-29 06:42:00 +0900

    fix calculate ratio value

--- src/main/code.ts
+++ src/main/code.ts
@@ -3,8 +3,14 @@ let selectedNode: SceneNode;
 figma.showUI(__html__, { width: 320, height: 280 });
 
 const gcd = (a: number, b: number): number => {
+  if (Number.isInteger(a) || Number.isInteger(b)) {return 1;}
   return b == 0 ? a : gcd(b, a % b);
 };
+
+const roundFloat = ( number: number, n: number ) => {
+  var _pow = Math.pow( 10 , n );
+  return Math.round( number * _pow ) / _pow;
+}
 
 const getData = (): void => {
   const node = figma.currentPage.selection[0];
@@ -12,13 +18,13 @@ const getData = (): void => {
     const { width, height } = node;
     const denom = gcd(width, height);
     const ratio = `${width / denom} : ${height / denom}`;
-    figma.ui.postMessage({ ratio, width, height });
+    figma.ui.postMessage({ ratio, width: roundFloat(width, 3), height: roundFloat(height, 3) });
   }
   selectedNode = node;
 };
 
-const setData = (width: number, height: number): void => {
-  selectedNode.resize(width, height);
+const resizeNode = (width: number, height: number): void => {
+  selectedNode.resize(roundFloat(width, 3), roundFloat(height, 3));
 };
 
 figma.on("selectionchange", () => {
@@ -27,7 +33,7 @@ figma.on("selectionchange", () => {
 
 figma.ui.onmessage = msg => {
   if (msg.type === "resize") {
-    setData(msg.width, msg.height);
+    resizeNode(msg.width, msg.height);
   }
 };
 
